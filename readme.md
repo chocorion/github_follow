c3a03de539e32000de35b15f400016676ab9cec3
rob <robiniscoding@gmail.com>
2020-03-21 22:47:19 +0100

    Add graph, readme and token

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Installation
+> Instruction for ubuntu, but works for a lot of distro
+
+```bash
+pip3 install matplotlib
+pip3 install networkx
+```
--- script.py
+++ script.py
@@ -2,6 +2,9 @@
 
 import requests, sys
 
+import matplotlib.pyplot as plt
+import networkx as nx
+
 baseUrl = "http://api.github.com/users/"
 firstUser = input("First user name : ")
 
@@ -11,13 +14,22 @@ except:
     print("Depth must be an int, greater than 0 !")
     sys.exit(1)
 
+try:
+    with open('token.txt') as file:
+        token = file.readline()
+except:
+    print("You need to create a file token.txt with a github token inside !")
 
+
+print(token)
+
+sys.exit(0)
 userTab = {
     firstUser: list()
 }
 
 def getUser(username):
-    r = requests.get(baseUrl + username)
+    r = requests.get(baseUrl + username, auth=('token', '0f3309802e6b1f8f346577761fb21ab81aea43ca'))
 
     if (r.status_code == 200):
         return r.json()
@@ -25,12 +37,22 @@ def getUser(username):
     return None
 
 def getFollowing(username):
-    r = requests.get(baseUrl + username + "/following")
+    r = requests.get(baseUrl + username + "/following", auth=('token', '0f3309802e6b1f8f346577761fb21ab81aea43ca'))
 
     if (r.status_code == 200):
         return r.json()
 
     return None
+
+
+def createGraph():
+    G = nx.MultiDiGraph()
+    
+    for user in userTab.keys():
+        for following in userTab[user]:
+            G.add_edge(user, following)
+
+    return G
 
 
 if __name__ == "__main__":
@@ -56,6 +78,12 @@ if __name__ == "__main__":
         
         current_user = new_user
 
-print(userTab)
-#follows = response.json()["following_url"]
-#print("follows : {}".format(follows))
+    graph = createGraph()
+    pos = nx.spring_layout(graph, iterations=10)
+    nx.draw(graph, pos, node_size=0, alpha=0.4, edge_color='r', font_size=16, with_labels=True)
+
+    plt.show()
+
+
+
+
